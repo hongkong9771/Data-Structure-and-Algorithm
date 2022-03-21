316d1afdee6e7306c967b462cf3318dfb1c67092
HongKong <hongkong9771@outlook.com>
2022-03-21 22:00:00 +0800

    complete the heap sort of ten sort

--- Code/Ten Sort/whk_sort.py
+++ Code/Ten Sort/whk_sort.py
@@ -113,15 +113,15 @@ class Sort(object):
             quick_sort(i+1, right)
             return nums
         return quick_sort(0, n-1)
-        
-        
+
+
     """
     非递归版本：Quick_Sort_2
     使用栈去存储每次待排序的区间索引
     """
     # 非递归版本快排
     # 单次区间排序
-    def quick_sort(left, right, nums):
+    def quick_sort(self, left, right, nums):
         index = random.randint(left, right)
         pivot = nums[index]
         nums[left], nums[index] = nums[index], nums[left]
@@ -136,7 +136,7 @@ class Sort(object):
         return i    # 分割区间的索引
 
 
-    def Quick_Sort_2(nums):
+    def Quick_Sort_2(self, nums):
         n = len(nums)
         s = []
         s.extend([0, n-1])
@@ -146,34 +146,81 @@ class Sort(object):
             if left >= right:
                 continue
             else:
-                mid = quick_sort(left, right, nums)
+                mid = self.quick_sort(left, right, nums)
                 s.extend([left, mid-1])
                 s.extend([mid+1, right])
         return nums
         
+    def Heap_Sort(self, nums):
+        """
+        7.堆排序
+        先建立一个最大堆（最大堆的定义为：树的顶点元素为整个数组的最大值，所有父节点的元素均大于其子节点），
+        这样最大的元素就处于堆顶，即整个数组中的第1位，然后将第1位的元素与最后1位相调换，则最大元素就在最后面了
+        （有点类似选择排序）。这样，最后一个元素就是有序的了，后面的所有操作均不包含最后一个有序的元素，接下来调整堆，
+        使其满足最大堆，然后再将堆顶元素与倒数第二个元素交换，则最后两个元素就是有序的了。以此类推，最终获得有序数组。
+
+        其中，使用数组存储堆时，有这样一条规则：以第i个元素为父节点的左右子节点在数组中的索引分别为：
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        注：可参考此链接：https://zhuanlan.zhihu.com/p/124885051
+        """
+        def adjust_heap(nums, node, n):
+            left = node * 2 + 1
+            if left < n:
+                right = left + 1
+                if right < n and nums[left] < nums[right]:
+                    left = right
+                if nums[node] < nums[left]:
+                    nums[left], nums[node] = nums[node], nums[left]
+                    """
+                    递归调用
+                    进行父节点与左右最大子节点之间的调换后，还需让调换后的
+                    left继续与其子节点进行判断，以确保满足最大堆的条件
+                    """
+                    adjust_heap(nums, left, n)
+
+        # 建立最大堆
+        n = len(nums)
+        for node in range(n // 2, -1, -1):
+            adjust_heap(nums, node, n)
+        # 调整堆
+        for i in range(n-1, -1, -1):
+            nums[i], nums[0] = nums[0], nums[i]
+            """
+            只需调整树的根节点处的元素与其子节点即可（当然，如果该父节点的子节点变动了，
+            则仍需递归地调用其子节点，adjust_heap已经写好了）满足最大堆。
+            """
+            adjust_heap(nums, 0, i)
+        return nums
 
 
 if __name__ == "__main__":
     nums = [23, 56, 3, 34, 12, 1, 13]
+    # nums = [5, 2, 3, 1]
     # nums = [1]
     s = Sort()
-    print("原始数组：", nums)
+    print("原始数组: ", nums)
     # 冒泡排序
     # nums_bubble_sort = s.Bubble_Sort(nums)
-    # print("冒泡排序后的数组：", nums_bubble_sort)
+    # print("冒泡排序后的数组: ", nums_bubble_sort)
 
     # 选择排序
     # nums_selection_sort = s.Selection_Sort(nums)
-    # print("选择排序后的数组：", nums_selection_sort)
+    # print("选择排序后的数组: ", nums_selection_sort)
 
     # 插入排序
     # nums_insertion_sort = s.Insertion_Sort(nums)
-    # print("插入排序后的数组：", nums_insertion_sort)
+    # print("插入排序后的数组: ", nums_insertion_sort)
 
     # 希尔排序
     # nums_shell_sort = s.Shell_Sort(nums)
-    # print("希尔排序后的数组：", nums_shell_sort)
+    # print("希尔排序后的数组: ", nums_shell_sort)
 
     # 快速排序
-    nums_quick_sort = s.Quick_Sort(nums)
-    print("快速排序后的数组：", nums_quick_sort)
+    # nums_quick_sort = s.Quick_Sort_2(nums)
+    # print("快速排序后的数组: ", nums_quick_sort)
+
+    # 堆排序
+    nums_heap_sort = s.Heap_Sort(nums)
+    print("堆排序后的数组: ", nums_heap_sort)
